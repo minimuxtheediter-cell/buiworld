a946596ff03dac9ee00871d881fb1cd14446f5bd
27105 <rowencbb@gmail.com>
2026-07-14 20:26:45 -0500

    Sha256ify the godwords so they work

---
+++ config/godwords.lua
@@ -0,0 +1,10 @@
+local godwords = {
+    "donotleakthisorelseyouarebanned"
+}
+local highkingwords = {
+    "welcomeold8291029475838475",
+    "ilovemonkechatingsiteandkr"
+}
+local lowkingwords = {
+    "noleakinglowkangwordorelseyouaredeleted"
+}
